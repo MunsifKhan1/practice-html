87b07a300c2b4e70f30255ed545f06af289a2dc7
MunsifKhan1 <munsifk597@gmail.com>
2024-10-14 22:37:37 -0700

    add initial files

---
+++ first.js
@@ -0,0 +1,21 @@
+// function declaration
+// function add(a, b) {
+//     return a + b;
+//   };
+//   console.log(add(6, 4));
+// function Expression
+//   const multiply = function(a, b) {
+//     return a * b;
+//   };
+//   console.log(multiply(6, 8)); 
+// Arrow function
+//   const add = (a, b) => a + b;
+// console.log(add(5, 4));
+// for of loop
+// const fruits = ['Apple', 'Banana', 'Mango', 'Orange'];
+
+// for (const fruit of fruits) {
+//   console.log(fruit.toUpperCase(fruit));
+// }
+// const square = num => num * num;
+// console.log(square(5));
